1a767c81b6cbe51733a3611424ae4dec028bd8c5
michbas796 <michal.b1990@gazeta.pl>
2014-06-03 14:19:41 +0200

    Podstawowe elementy GUI dla administratora.
    Przygotowałem szkielet GUI i obsługę logowania.

---
+++ Administrator/Administrator/LoginWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Security.Cryptography;
+
+namespace Administrator
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        private byte[] _hash;
+        private string _login = "";
+
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        public string Login
+        {
+            get
+            {
+                return _login;
+            }
+        }
+
+        private void LogInButton_Click(object sender, RoutedEventArgs e)
+        {
+            DBClient.DBClient db = new DBClient.DBClient();  // klient bazy danych
+            HashAlgorithm sha = HashAlgorithm.Create("SHA512");
+            byte[] passwordBytes = System.Text.Encoding.ASCII.GetBytes(passwordBox.Password);
+            _hash = sha.ComputeHash(passwordBytes);
+            _login = loginTextBox.Text;
+
+            //Sprawdzanie czy w bazie istnieje podany użytkownik
+            if (db.FindUser(_login, _hash) == true)
+            {
+                this.DialogResult = true;
+            }
+            else
+            {
+                this.DialogResult = false;
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (loginTextBox.Text != "" && passwordBox.Password != "")
+            {
+                logInButton.IsEnabled = true;
+            }
+            else
+            {
+                logInButton.IsEnabled = false;
+            }
+        }
+
+        private void PasswordBox_TextChanged(object sender, RoutedEventArgs e)
+        {
+            if (loginTextBox.Text != "" && passwordBox.Password != "")
+            {
+                logInButton.IsEnabled = true;
+            }
+            else
+            {
+                logInButton.IsEnabled = false;
+            }
+        }
+    }
+}
---
+++ Administrator/Administrator/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Administrator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DBClient.DBClient db;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            while (true)
+            {
+                if (LogIn() == true)
+                {
+                    db = new DBClient.DBClient();
+
+                    //TODO: dopisać resztę (podobnie jak w kliencie rejestratorki i lekarza).
+                    break;
+                }
+            }
+        }
+
+        private void aboutMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            AboutDialog aboutDialog = new AboutDialog();
+            aboutDialog.ShowDialog();
+        }
+
+        private void logoutMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = System.Windows.Visibility.Hidden;
+            //TODO: wylogowanie z bazy danych            
+            if (LogIn() == true)
+            {
+                //TODO: ponowne zalogowanie
+                this.Visibility = System.Windows.Visibility.Visible;
+            }
+            else
+            {
+                Environment.Exit(0);
+            }
+        }
+
+        private bool LogIn()
+        {
+            LoginWindow loginWindow = new LoginWindow();
+            bool? result = loginWindow.ShowDialog();
+            if (result == true)
+            {
+                return true;
+            }
+            else if (result == null) //zamknięcie okna logowania, lub poważny błąd
+            {
+                Environment.Exit(0);
+            }
+            return false;
+        }
+    }
+}
